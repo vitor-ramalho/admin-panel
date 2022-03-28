7dd54854309661a027257acd9035ab63bb45c17b
vitor-ramalho <39497088+vitor-ramalho@users.noreply.github.com>
2022-03-28 16:37:03 -0300

    Criacao Sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Sidebar from "./components/Sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
+import "./app.css"
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="others"> Other Pages</div>
+      </div>
+
     </div>
   );
 }
